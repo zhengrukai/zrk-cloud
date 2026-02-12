bb1933ce2cdca083e754a91d1602bd4f1e5c0184
zhengrukai <zhengrukai@no-email.com>
2026-02-12 13:07:45 +0800

    新增基础设施组件中的MQ模块

---
+++ zrk-framework/zrk-spring-boot-starter-mq/src/main/java/cn/zrkcoder/cloud/framework/mq/rabbitmq/config/ZrkRabbitMQAutoConfiguration.java
@@ -0,0 +1,27 @@
+package cn.zrkcoder.cloud.framework.mq.rabbitmq.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * RabbitMQ 消息队列配置类
+ *
+ * @author zrk on 2026/2/12
+ */
+@AutoConfiguration
+@Slf4j
+@ConditionalOnClass(name = "org.springframework.amqp.rabbit.core.RabbitTemplate")
+public class ZrkRabbitMQAutoConfiguration {
+
+    /**
+     * Jackson2JsonMessageConverter Bean：使用 jackson 序列化消息
+     */
+    @Bean
+    public MessageConverter createMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
