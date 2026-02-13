04ad93b4d35105d58af5479af1c63702464bef41
zhengrukai <zhengrukai@no-email.com>
2026-02-13 12:05:57 +0800

    新增基础设施组件中的test模块

---
+++ zrk-framework/zrk-spring-boot-starter-test/src/main/java/cn/zrkcoder/cloud/framework/test/core/ut/BaseMockitoUnitTest.java
@@ -0,0 +1,13 @@
+package cn.zrkcoder.cloud.framework.test.core.ut;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * 纯 Mockito 的单元测试
+ *
+ * @author zrk on 2026/2/13
+ */
+@ExtendWith(MockitoExtension.class)
+public class BaseMockitoUnitTest {
+}
